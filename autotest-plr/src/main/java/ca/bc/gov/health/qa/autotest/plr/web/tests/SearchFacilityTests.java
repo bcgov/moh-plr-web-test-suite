16c5a8493af52b7c64d55d858d48742e6e3e3496
Alexei Marykuca-Vidal <alexei.marykuca-vidal@cgi.com>
2025-10-20 16:45:26 -0700

    Refactor: Expand on Maximum Results test case, move warning into JSON file

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/SearchFacilityTests.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/SearchFacilityTests.java
@@ -406,6 +406,27 @@ public class SearchFacilityTests implements SimpleTest {
     }
 
     @Test
+    // F1-015. Maximum Search Results
+    public void testMaximumResults()
+    {
+        final int expectedResults = 20;
+        final String maxResultsWarning = warningList.getString("maximumResults");
+
+        SearchFacilityPage searchFacility = navigateToSearchFacilityPage(UserType.ADMIN);
+        SearchFacilityResultsFragment searchResults = searchFacility.searchByCriteria(
+                "A*", "", "", "", null,
+                "Select One", "", null, false);
+        List<String> warningMessageList = searchFacility.waitForAlertMessagesFragment().grabWarningMessageList();
+
+        assertEquals(searchResults.grabResultsRowCount(), expectedResults,
+                "Returned search result does not match the expected maximum number of search results.");
+        assertTrue(warningMessageList.contains(maxResultsWarning),
+                "Maximum search results warning not displayed.");
+        assertTrue(searchResults.getFormResults().contains(String.format("%d results", expectedResults)),
+                "Form result does not match expected maximum search results.");
+    }
+
+    @Test
     // Search Facility: Facility Search with Wildcard - Match Ending Character
     public void testCriteriaSearchWildcardMatchEnding()
     {
@@ -435,23 +456,6 @@ public class SearchFacilityTests implements SimpleTest {
 
         assertTrue(searchResults.grabResultsRowCount() > 0, "Searching for facility with wildcard to match all but starting character results in no facilities being returned.");
         assertTrue(searchResults.getResultsRow(0).getFirst().startsWith(expectedFacilityName), "Returned facility doesn't have the expected facility name used in search.");
-    }
-
-    @Test
-    // Search Facility: Maximum Results
-    public void testMaximumResults()
-    {   final int expectedResults = 20;
-        final String expectedMessage = "Maximum search results returned. Please refine your search criteria.";
-        PlrWebWorkflow workflow = workflowManager_.getSelectedWorkflow();
-        SearchFacilityPage searchFacility = workflow.getPlrWebAccessActions().openSearchFacility();
-        SearchFacilityResultsFragment searchResults = searchFacility.searchByCriteria(
-                "A*", "", "", "", null,
-                "Select One", "", null, false);
-        List<String> warningMessageList = searchFacility.waitForAlertMessagesFragment().grabWarningMessageList();
-
-        assertEquals(searchResults.grabResultsRowCount(), expectedResults, "Returned search result row count does not match the expected number of search results.");
-        assertTrue(warningMessageList.contains(expectedMessage), "Warning message for maximum search results not returned.");
-        assertTrue(searchResults.getFormResults().contains("20 results"), "Form result does not match expected maximum search results.");
     }
 
     @Test
