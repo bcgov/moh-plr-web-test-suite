9ade705b317a8572e6f56f6eaf4f29fd87fa159d
Alexei Marykuca-Vidal <alexei.marykuca-vidal@cgi.com>
2025-12-10 13:08:59 -0800

    Fix Street Types test case street type handling

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/CreateFacilityComplexTests.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/CreateFacilityComplexTests.java
@@ -563,6 +563,7 @@ public class CreateFacilityComplexTests implements SimpleTest {
             addFacility.clickNext("Facility", "");
 
             String fullAddress = fillOutAddressSection(addFacility, addressLine, 5);
+            fullAddress = fullAddress.substring(0, fullAddress.indexOf(",")) + " " + streetTypes.get(streetTypeIndex);
 
             addFacility.waitForAddFacilityStep("Address", false);
             ViewFacilityPage newFacility = addFacility.getFacilitySummary().clickSubmitButton();
