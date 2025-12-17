d7a5cfdfe7f065e9d5a2824abd966c8beb243ce3
Alexei Marykuca-Vidal <alexei.marykuca-vidal@cgi.com>
2025-12-17 13:31:56 -0800

    Add plans/start of F4-045

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/facility/UpdateFacilityComplexTests.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/facility/UpdateFacilityComplexTests.java
@@ -1,15 +1,26 @@
 package ca.bc.gov.health.qa.autotest.plr.web.tests.facility;
 
 import ca.bc.gov.health.qa.autotest.plr.fhir.FHIRController;
+import ca.bc.gov.health.qa.autotest.plr.fhir.data.FacilityMaintainConfig;
+import ca.bc.gov.health.qa.autotest.plr.fhir.data.OrganizationMaintainConfig;
+import ca.bc.gov.health.qa.autotest.plr.fhir.maintain.MaintainFacilityBuilder;
+import ca.bc.gov.health.qa.autotest.plr.fhir.maintain.MaintainOrgBuilder;
 import ca.bc.gov.health.qa.autotest.plr.util.UserType;
+import ca.bc.gov.health.qa.autotest.plr.web.actions.facility.UpdateFacilitySimpleActions;
+import ca.bc.gov.health.qa.autotest.plr.web.actions.facility.ViewFacilityActions;
+import ca.bc.gov.health.qa.autotest.plr.web.pages.plr.facility.UpdateFacilityPage;
 import ca.bc.gov.health.qa.autotest.plr.web.workflows.PlrWebWorkflow;
 import ca.bc.gov.health.qa.autotest.plr.web.workflows.PlrWebWorkflowManager;
 import ca.bc.gov.health.qa.autotest.runner.util.log.ExecutionLogManager;
 import ca.bc.gov.health.qa.autotest.runner.util.testng.SimpleTest;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static ca.bc.gov.health.qa.autotest.plr.web.tests.TestHelper.generateAlphabetString;
 
 public class UpdateFacilityComplexTests implements SimpleTest
 {
@@ -20,6 +31,7 @@ public class UpdateFacilityComplexTests implements SimpleTest
 
     public UpdateFacilityComplexTests() {}
 
+    /*
     @AfterClass
     public void teardown()
     {
@@ -27,6 +39,7 @@ public class UpdateFacilityComplexTests implements SimpleTest
         workflowManager_.logoutAllAndClose();
         LOG.info("Done.");
     }
+     */
 
     @BeforeTest
     public void beforeTest()
@@ -42,5 +55,52 @@ public class UpdateFacilityComplexTests implements SimpleTest
         {
             workflow.login().openPlr();
         }
+    }
+
+    @Test
+    // F4-022. Rejection of Non-Acceptable Characters
+    public void rejectionNonAcceptableCharacters()
+    {
+        // attempt to add identifier
+        // attempt to update identifier
+        // attempt to add name
+        // attempt to update name
+        // attempt to update civic address
+        // attempt to update other address
+        // attempt to add new telecom
+        // attempt to update telecom
+        // attempt to add e-address
+        // attempt to update e-address
+        // attempt to add note
+        // attempt to update note
+    }
+
+    @Test
+    // F4-045. Generating Internal Relationship Identifier (RID)
+    public void generateRelationshipIdentifier()
+    {
+        final UpdateFacilitySimpleActions actions = workflowManager_.getSelectedWorkflow().getUpdateFacilitySimpleActions();
+        final FacilityMaintainConfig config = new FacilityMaintainConfig().withAllAttributes(1,0);
+        final MaintainFacilityBuilder facility = fhirController.createFacility(config);
+
+        final OrganizationMaintainConfig orgConfig = new OrganizationMaintainConfig();
+        final MaintainOrgBuilder org1 = fhirController.createOrganization(orgConfig.withName(generateAlphabetString(15)));
+        final MaintainOrgBuilder org2 = fhirController.createOrganization(orgConfig.withName(generateAlphabetString(15)));
+
+        UpdateFacilityPage page = actions.openFacility(facility);
+        // create organization relationship
+        // check new relationship identifier
+        // create new organization relationship
+        // verify relationship identifier has changed + incremented
+        /*
+            create facility
+            create org with unique name
+            go to new facility
+            create org relationship with new org
+            refresh page + note down rel identifier
+            create org with new name again
+            create another org relationship
+            assert new rel identifier > previous identifier in order
+         */
     }
 }
