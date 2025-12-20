124c36beee7ce06907d183f2b47326866ae605c9
Alexei Marykuca-Vidal <alexei.marykuca-vidal@cgi.com>
2025-12-19 17:29:58 -0800

    Remove unnecessary import

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/facility/ViewFacilityComplexTests.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/tests/facility/ViewFacilityComplexTests.java
@@ -24,7 +24,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.reporters.jq.Main;
 
 import java.util.*;
 
