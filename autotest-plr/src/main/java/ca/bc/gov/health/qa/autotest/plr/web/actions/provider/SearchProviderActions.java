39b9c548a6bf48c7c6b8229dbb7f283ef00c6069
Alexei-cgi <alexei.marykuca-vidal@cgi.com>
2026-01-27 10:45:59 -0800

    Move SearchProviderActions

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/actions/provider/SearchProviderActions.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/web/actions/provider/SearchProviderActions.java
@@ -5,18 +5,16 @@ import ca.bc.gov.health.qa.autotest.plr.web.pages.plr.provider.ViewProviderPage;
 import ca.bc.gov.health.qa.autotest.runner.util.selenium.SeleniumSession;
 
 /**
- * TODO (AZ) - doc
+ * Actions class for the Search Provider page/functions
  */
 public class SearchProviderActions
 {
     private final SeleniumSession selenium_;
 
     /**
-     * TODO (AZ) - doc
+     * Initializes class and SeleniumSession.
      *
-     * @param selenium
-     *        ???
-     *
+     * @param selenium  the current SeleniumSession
      */
     public SearchProviderActions(SeleniumSession selenium)
     {
@@ -24,12 +22,10 @@ public class SearchProviderActions
     }
 
     /**
-     * TODO (AZ) - doc
+     * Opens the view provider page in the table of search results at the given index.
      *
-     * @param index
-     *        ???
-     *
-     * @return ???
+     * @param index     the index of the search result to open
+     * @return          the ViewProviderPage object for the associated facility
      */
     public ViewProviderPage openSearchResults(int index)
     {
