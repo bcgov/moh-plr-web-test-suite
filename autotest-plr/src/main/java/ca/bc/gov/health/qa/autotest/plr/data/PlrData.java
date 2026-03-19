5348f4daa27ef29cf02cff655fb4d1bf7ed8eaf7
Alexei-cgi <alexei.marykuca-vidal@cgi.com>
2026-03-19 13:21:29 -0700

    Attempt to add back organization/individual relationship generation

--- autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/data/PlrData.java
+++ autotest-plr/src/main/java/ca/bc/gov/health/qa/autotest/plr/data/PlrData.java
@@ -163,7 +163,7 @@ public class PlrData
         final String pracType = providerType.equals(ProviderType.BC_PRACTITIONER) ? "BC" : "OOP";
         final IndividualRoleType roleType = pracType.equals("BC") ? IndividualRoleType.DEN : IndividualRoleType.OOP_DEN;
 
-        final String defaultName = pracType.equals("BC") ? "TestBCPrac2" : "TestOOPPrac2";
+        final String defaultName = pracType.equals("BC") ? "TestBCPract" : "TestOOPPract";
         final String minimumName = pracType.equals("BC") ? "MinimumDataBCPrac" : "MinimumDataOOPPrac";
 
         MaintainIndividualBuilder defaultBuilder = factory.build(new IndividualMaintainConfig(roleType)
@@ -191,25 +191,25 @@ public class PlrData
         if (defaultQuery.isEmpty())
         {
             // Create Organization/Individual relationships
-            //String orgRelIdentifier = fhir.createOrganization(OrganizationDataGenerator.getInstance().randomOrgRoleType())
-            //        .getIdentifier(IdentifierType.IPC);
-            //defaultBuilder = defaultBuilder.addOrganizationRelationship(
-            //        IdentifierType.IPC, orgRelIdentifier, dataGen.generatePractitionerRelationshipCode());
+            String orgRelIdentifier = fhir.createOrganization(OrganizationDataGenerator.getInstance().randomOrgRoleType())
+                    .getIdentifier(IdentifierType.IPC);
+            defaultBuilder = defaultBuilder.addOrganizationRelationship(
+                    IdentifierType.IPC, orgRelIdentifier, dataGen.generatePractitionerRelationshipCode());
 
-            //orgRelIdentifier = fhir.createOrganization(OrganizationDataGenerator.getInstance().randomOrgRoleType())
-            //        .getIdentifier(IdentifierType.IPC);
-            //defaultBuilder = defaultBuilder.addOrganizationRelationship(
-            //        IdentifierType.IPC, orgRelIdentifier, dataGen.generatePractitionerRelationshipCode());
+            orgRelIdentifier = fhir.createOrganization(OrganizationDataGenerator.getInstance().randomOrgRoleType())
+                    .getIdentifier(IdentifierType.IPC);
+            defaultBuilder = defaultBuilder.addOrganizationRelationship(
+                    IdentifierType.IPC, orgRelIdentifier, dataGen.generatePractitionerRelationshipCode());
 
-            //String indRelIdentifier = fhir.createIndividual(dataGen.randomRoleType(false))
-            //        .getIdentifier(IdentifierType.IPC);
-            //defaultBuilder = defaultBuilder.addIndividualRelationship(
-            //        IdentifierType.IPC, indRelIdentifier, dataGen.generatePractitionerRelationshipCode());
+            String indRelIdentifier = fhir.createIndividual(dataGen.randomRoleType(false))
+                    .getIdentifier(IdentifierType.IPC);
+            defaultBuilder = defaultBuilder.addIndividualRelationship(
+                    IdentifierType.IPC, indRelIdentifier, dataGen.generatePractitionerRelationshipCode());
 
-            //indRelIdentifier = fhir.createIndividual(dataGen.randomRoleType(false))
-            //        .getIdentifier(IdentifierType.IPC);
-            //defaultBuilder = defaultBuilder.addIndividualRelationship(
-            //        IdentifierType.IPC, indRelIdentifier, dataGen.generatePractitionerRelationshipCode());
+            indRelIdentifier = fhir.createIndividual(dataGen.randomRoleType(false))
+                    .getIdentifier(IdentifierType.IPC);
+            defaultBuilder = defaultBuilder.addIndividualRelationship(
+                    IdentifierType.IPC, indRelIdentifier, dataGen.generatePractitionerRelationshipCode());
 
             defaultIndiv = fhir.queryIndividualByIdentifier(roleType.getIdentifierType(), fhir.submitIndividual(defaultBuilder).getIdentifier(roleType.getIdentifierType()));
         } else defaultIndiv = defaultQuery.getFirst();
